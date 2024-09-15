e74b17fa5e6342cab65c2e63268905becb19d249
Michał Klepner <michal.klepner1@o2.pl>
2024-09-15 15:04:08 +0200

    Nested routing and Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,15 @@ function App() {
         <Routes>
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
-          <Route path="/" element={<Homepage />} />
+          <Route index element={<Homepage />} />
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
-          <Route path="app" element={<AppLayout />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<p>List of Cities</p>} />
+            <Route path="cities" element={<p>List of cities</p>} />
+            <Route path="countries" element={<p>List of countries</p>} />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>

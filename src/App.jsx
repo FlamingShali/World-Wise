994ae757d8a83d7ffb09d5c9a561b9f0c9437e56
Michał Klepner <michal.klepner1@o2.pl>
2024-09-16 11:26:09 +0200

    Navigate Component for starter page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import { useEffect, useState } from "react";
@@ -10,7 +10,7 @@ import City from "./components/City";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
-import Form from "./components/Form"
+import Form from "./components/Form";
 function App() {
   const BASE_URL = "http://localhost:8000/cities";
   const [cities, setCities] = useState([]);
@@ -41,10 +41,7 @@ function App() {
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
           <Route path="app" element={<AppLayout />}>
-            <Route
-              index
-              element={<CityList cities={cities} isLoading={isLoading} />}
-            />
+            <Route index element={<Navigate replace to="cities" />} />
             <Route
               path="cities"
               element={<CityList cities={cities} isLoading={isLoading} />}

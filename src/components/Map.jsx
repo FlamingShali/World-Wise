bbbd576e05cd876f99936c67f068df033ccb459e
Michał Klepner <michal.klepner1@o2.pl>
2024-09-17 18:37:03 +0200

    Button 'get your location'

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -34,11 +34,16 @@ const Map = () => {
     },
     [mapLat, mapLng]
   );
+
+  useEffect(function(){
+    if(geolocationPosition) setMapPosition([geolocationPosition.lat, geolocationPosition.lng])
+  },[geolocationPosition])
+
   return (
     <div className={styles.mapContainer}>
-      <Button type="position" onClick={getPosition}>
+      {!geolocationPosition && <Button type="position" onClick={getPosition}>
         {isLoadingPosition ? "Loading..." : "Use your position"}
-      </Button>
+      </Button>}
       <MapContainer
         center={mapPosition}
         zoom={6}

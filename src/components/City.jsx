d63c47d9bf417b7298b06c160f8afe5f47bf9e7a
Michał Klepner <michal.klepner1@o2.pl>
2024-09-17 03:10:26 +0200

    marker and dynamic location url latlng

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,10 +1,9 @@
-import { useParams } from "react-router-dom";
-import styles from "./City.module.css";
-
 import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { useCities } from "../contexts/CitiesContext";
-import Spinner from "./Spinner";
 import BackButton from "./BackButton";
+import styles from "./City.module.css";
+import Spinner from "./Spinner";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -18,17 +17,17 @@ function City() {
   const { id } = useParams();
   const { getCity, currentCity, isLoading } = useCities();
 
- 
-
   useEffect(
     function () {
       getCity(id);
     },
-    [id]
+    [id, getCity]
   );
 
   const { cityName, emoji, date, notes } = currentCity;
+
   if (isLoading) return <Spinner />;
+
   return (
     <div className={styles.city}>
       <div className={styles.row}>
@@ -61,7 +60,9 @@ function City() {
         </a>
       </div>
 
-      <div><BackButton/></div>
+      <div>
+        <BackButton />
+      </div>
     </div>
   );
 }
